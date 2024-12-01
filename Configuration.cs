4252cf44c62305ef1957df1d9485945bd9714ea8
alex-mejik <gocluber@gmail.com>
2024-12-01 13:27:49 +0200

    Add files via upload

---
+++ Configuration.cs
@@ -0,0 +1,25 @@
+﻿namespace DB_TEST.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DB_TEST.AppDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(DB_TEST.AppDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+
+        }
+    }
+}
