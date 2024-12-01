8ab7f1be65f6369d00330860a6311b8bf892a32a
alex-mejik <gocluber@gmail.com>
2024-12-01 13:00:38 +0200

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using DB_TEST.Migrations;
+using System.Data.Entity;
+
+namespace DB_TEST
+{
+
+
+   
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+            : base("name=AppDbContext") // Connection string name in config
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<AppDbContext, Configuration>());
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Configure relationships and constraints (optional)
+            modelBuilder.Entity<Product>()
+                .HasRequired(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+
+
+}
